8ffa6d82b0bead63751fe51cfafcba545f9bf375
Lilmeep22 <112208032+Lilmeep22@users.noreply.github.com>
2023-02-19 19:30:33 -0600

    Updated mobile node program for loopback tsting

--- LoRa/mobileNode.py
+++ LoRa/mobileNode.py
@@ -14,10 +14,20 @@ import adafruit_ssd1306
 # Import RFM9x library
 import adafruit_rfm9x
 
-# Configure reset button
-reset_button = DigitalInOut(board.D5)
-reset_button.direction = Direction.INPUT
-reset_button.pull = Pull.UP
+# Button A
+btnA = DigitalInOut(board.D5)
+btnA.direction = Direction.INPUT
+btnA.pull = Pull.UP
+
+# Button B
+btnB = DigitalInOut(board.D6)
+btnB.direction = Direction.INPUT
+btnB.pull = Pull.UP
+
+# Button C
+btnC = DigitalInOut(board.D12)
+btnC.direction = Direction.INPUT
+btnC.pull = Pull.UP
 
 # Initialize I2C interface.
 i2c = busio.I2C(board.SCL, board.SDA)
@@ -45,48 +55,90 @@ rfm9x.coding_rate = 8 #[5, 6, 7, 8] - Higher = more FEC, lower = more throughput
 rfm9x.spreading_factor = 12 #[6, 7, 8, ... 10, 11, 12] - Higher = better SNR tolerance, lower = more throughput
 rfm9x.enable_crc = True
 
-sent_packets = 0
-received_packets = 0
-last_rx_rssi = 0
-last_rx_snr = 0
-updateDisplay = True
+# Ping Function
+def ping():
+    # ping the fixed node
+    tx_data = bytes("ping", "utf-8")
+    rfm9x.send(tx_data)
 
-last_tx_time = time.time()
-pingTime = 1 # Time in seconds to wait between pings
+    linkStats = [0,-999, -999]
 
-# Send a reset
-tx_data = bytes("RST!", "utf-8")
-rfm9x.send(tx_data)
-
-while True:
-    # Send reset if we need to
-    if not reset_button.value:
-        sent_packets = 0
-        received_packets = 0
-        last_rx_rssi = 0
-        last_rx_snr = 0
-        updateDisplay = True
-        tx_data = bytes("RST!", "utf-8")
-        rfm9x.send(tx_data)
-        continue
-
-    # Time to ping?
-    if(time.time() - last_tx_time <= pingTime):
-        # too soon, check back l8r
-        continue
-
-    # Send packet to remote, increment count
-    rfm9x.send(bytes("hi mom","utf-8"))
-    sent_packets += 1
-    updateDisplay = True
-
-    # Get response from remote
+    # Wait for a reply, timeout if necessary
     packet = rfm9x.receive()
     if(packet):
-        # get info, get stats
-        received_packets = str(packet, "utf-8")
-        last_rx_rssi = rfm9x.last_rssi
-        last_rx_snr = rfm9x.last_snr
+        linkStats[0] = 1
+        linkStats[1] = rfm9x.last_rssi
+        linkStats[2] = rfm9x.last_snr
+
+    # Return link stats
+    return linkStats
+
+# Link Quality Checker
+def checkLink():
+    linkQualities = []
+
+    # Ping 10 times, append to list
+    for i in range(10):
+        linkQualities.append(ping())
+
+    # Return packet list
+    return linkQualities
+
+# Main
+updateDisplay = True
+while True:
+    # Reset/Clear Screen
+    if not btnA.value:
+        display.fill(0)
+        display.text('Waiting...', 15, 0, 1)
+        display.text('', 15, 20, 1)
+        display.show()
+        continue
+
+    # Single ping?
+    if not btnB.value:
+        res = ping()
+        display.fill(0)
+        display.text('Success: {}'.format(res[0]), 15, 0, 1)
+        display.text('RSSI: {}, SNR: {}'.format(res[1], res[2]), 15, 20, 1)
+        display.show()
+        continue
+
+    # do a test
+    if not btnC.value:
+        # Get our 10 test values
+        res = checkLink()
+
+        # variables to hold test stats
+        avgRSSI = 0
+        avgSNR = 0
+        successfulPings = 0
+
+        # Iterate over list, get values of interest
+        for row in res:
+            if(row[0] == 1):
+                successfulPings += 1
+                avgRSSI += row[1]
+                avgSNR += [2]
+            else:
+                continue
+
+        # more than 1 ping, average the RSSI/SNR
+        if(successfulPings > 1):
+            avgRSSI /= successfulPings
+            avgSNR /= successfulPings
+
+        # No pings?
+        if(successfulPings == 0):
+            avgRSSI = 0
+            avgSNR = 0
+
+        # Update display
+        display.fill(0)
+        display.text('Pings: {}'.format(successfulPings), 15, 0, 1)
+        display.text('RSSI: {}, SNR: {}'.format(avgRSSI, avgSNR), 15, 20, 1)
+        display.show()
+        continue
 
     if(updateDisplay):
         display.fill(0)
